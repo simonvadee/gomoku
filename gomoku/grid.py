620e4a3c48809c3e56ee498a6bb02a984dd2f3f7
Simon Vadée <you@example.com>
2015-11-24 16:42:16 +0100

    fix highlight background

--- gomoku/grid.py
+++ gomoku/grid.py
@@ -33,7 +33,7 @@ class GridManager(object):
 
         else:
             print("error case")
-
+            raise
 
     # TODO :
     # implement functions check row, column, diagonal
@@ -51,10 +51,13 @@ class GridGui(object):
         
         def button_command(x,y):
             if self._playing is True:
-                self._grid[x, y] = self._grid._turn
-                print("pop")
+                try:
+                    self._grid[x, y] = self._grid._turn
+                except Exception as e:
+                    return
                 self._playing = False
-                self._cases[x, y].config(background=("black" if self._grid._turn == 1 else "red"))
+                background = ("black" if self._grid._turn == 1 else "red")
+                self._cases[x, y].config(background=background, activebackground=background)
             
         self._playing = False
         self._grid = grid
@@ -62,7 +65,7 @@ class GridGui(object):
         self._cases = numpy.empty_like(grid._grid, dtype='object')
         for i in range(self._grid._height):
             for j in range(self._grid._width):
-                setCase = self._cases[i,j] = tk.Button(canvas, command=lambda x=i, y=j: button_command(x,y), background="white")
+                setCase = self._cases[i,j] = tk.Button(canvas, command=lambda x=i, y=j: button_command(x,y), background="white", activebackground="white")
                 setCase.grid(row=i, column=j)
 
     def reset(self):
