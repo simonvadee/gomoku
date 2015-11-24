8105ac69c20381b2144a31bcf92398f7da0a2192
Poulpy <aure.girardeau@gmail.com>
2015-11-24 19:55:16 +0100

    breakable rule and end ok

--- gomoku/grid.py
+++ gomoku/grid.py
@@ -81,10 +81,45 @@ class GridManager(object):
                 return winner
             
             self._gridGui.update()
-   # TODO :
+
+    # TODO :
     # implement functions check row, column, diagonal
 
+    def isBreakable(self, enemyWay, coord, enemy, friendWay) :
+        x = coord[1] + enemyWay[0]
+        y = coord[0] + enemyWay[1]
+        isBreakable = True
+        count = 1
+        while isBreakable :
+            isBreakable = False
+            if y in range(0, self._height) and x in range(0, self._width) :
+                if self._grid[(y, x)] == 0 :
+                    break
+                elif self._grid[(y, x)] == enemy :
+                    return 0
+                elif self._grid[(y, x)] == -enemy :
+                    if enemyWay == tuple(scalar * (-1) for scalar in friendWay) :
+                        count += 1
+                    isBreakable = True
+            x += enemyWay[0]
+            y += enemyWay[1]
+        return count
+    
+    def checkAround(self, coord, valid, enemy, friendWay) :
+        for y in range(coord[0] - 1, coord[0] + 2) :
+            for x in range(coord[1] - 1, coord[1] + 2) :
+                if y in range(0, self._height) and x in range(0, self._width) and self._grid[(y, x)] == enemy :
+                    enemyWay = (coord[1] - x, coord[0] - y)
+                    return self.isBreakable(enemyWay, coord, enemy, friendWay)
+        return False
+
     def recursDir(self, way, lastMove, valid) :
+        ret = self.checkAround(lastMove[0], valid, -lastMove[1], way)
+        if ret :
+            return -ret
+        valid.append(lastMove)
+        # check Breakable+
+            # determiner way pour checker les breaks
         x = lastMove[0][1] + way[0]
         y = lastMove[0][0] + way[1]
         if y in range(0, self._height) and x in range(0, self._width) and self._grid[(y, x)] == lastMove[1] :
@@ -93,7 +128,7 @@ class GridManager(object):
         return 0
 
     def findWinner(self):
-        valid = [self._lastMove[0]]
+        valid = []
         for elem in self._dir :
             res = 1 + self.recursDir(elem, self._lastMove, valid) + self.recursDir(tuple(scalar * (-1) for scalar in elem), self._lastMove, valid)
             if res >= 5 :
