bca94c662896e58be4fa3435e02cdb82d01c2b63
Simon Vadée <simon.vadee@epitech.eu>
2016-01-11 11:07:02 +0100

    ident

--- gomoku/player.py
+++ gomoku/player.py
@@ -3,8 +3,8 @@ class Player(object):
     def __init__(self, gridGui, img=None):
         self._gridGui = gridGui
         self._img = img
-
-   def play(self):
+        
+    def play(self):
         self._gridGui._playing = True
         while self._gridGui._playing is True:
             self._gridGui.update()
