8a5e8b8e7cbc44ef1b3b20210182a12bcc442ffa
Simon Vadée <simon.vadee@epitech.eu>
2016-01-12 16:36:33 +0100

    getter

--- v2/Board.cpp
+++ v2/Board.cpp
@@ -1,8 +1,11 @@
 # include "Board.hh"
 
-Board::Board(unsigned int size) : _board(new int[size * size])
+Board::Board(unsigned int size) : _board(new std::vector<int>(size * size, 0))
 {
-
+  _dir.push_back(std::pair<unsigned int, unsigned int>(1, 0));
+  _dir.push_back(std::pair<unsigned int, unsigned int>(0, 1));
+  _dir.push_back(std::pair<unsigned int, unsigned int>(-1, 1));
+  _dir.push_back(std::pair<unsigned int, unsigned int>(-1, -1));
 }
 
 Board::~Board()
@@ -10,6 +13,10 @@ Board::~Board()
 
 }
 
+std::vector<int>*		Board::getBoard() const
+{
+  return _board;
+}
 
 int		Board::getAlignement(unsigned int pos, std::pair<unsigned int, unsigned int> dir, PLAYER player)
 {
@@ -23,5 +30,13 @@ bool		Board::doubleThreeRule(unsigned int pos, PLAYER player)
 
 int		Board::move(unsigned int pos, PLAYER player)
 {
-  this->_board[pos] = static_cast<int>(player);
+  (*this->_board)[pos] = static_cast<int>(player);
+}
+
+bool		Board::isWinner()
+{
+  return (this->getAlignement(_lastMove, _dir[HORIZONTAL], _lastPlayer) == 5
+	  || this->getAlignement(_lastMove, _dir[VERTICAL], _lastPlayer) == 5
+	  || this->getAlignement(_lastMove, _dir[DIAGONAL_LR], _lastPlayer) == 5
+	  || this->getAlignement(_lastMove, _dir[DIAGONAL_RL], _lastPlayer) == 5);
 }
