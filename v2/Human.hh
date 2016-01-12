1cd94dd0de2bfe96848b584715e9b0e1eb227f93
Simon Vadée <simon.vadee@epitech.eu>
2016-01-12 15:51:25 +0100

    v2

---
+++ v2/Human.hh
@@ -0,0 +1,18 @@
+#ifndef HUMAN_H_
+# define HUMAN_H_
+
+# include "Player.hh"
+
+class Human : public Player
+{
+public:
+  Human();
+  ~Human();
+
+  bool		play();
+
+private:
+
+}
+
+#endif /* !HUMAN_H_ */
---
+++ v2/gomoku.cpp
@@ -0,0 +1,15 @@
+# include "Board.hh"
+# include "IA.hh"
+# include "Human.hh"
+
+void		lauchGame(Board *board, Player *pl1, Player *pl2)
+{
+
+}
+
+int		main()
+{
+  GUI gui;
+
+  gui.startMenu()
+}
